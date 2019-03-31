b28481e7bbebfe78f3ea5e45e082040ab31e8542
Luis Antonio Rojas Ramirez <33220687+sikic@users.noreply.github.com>
2019-03-31 20:24:58 +0200

    producto

---
+++ JavaScriptEjercicios/ejer1.js
@@ -0,0 +1,27 @@
+function producto(x,y){
+    if(typeof(x) == "number" && typeof(y)=="number")
+        return x*y;
+    else if((typeof(x) == "number" && typeof(y)=="object") || (typeof(x) == "object" && typeof(y)=="number")){
+        if(typeof(x)=="object"){
+            for (let index = 0; index < x.length; index++)
+              x[index] *= y;
+            
+              return x;
+        }else{
+           for (let index = 0; index < y.length; index++)
+             y[index] *= x;
+               
+             return y;
+           }
+
+        }
+        else if((typeof(x) == "object" && typeof(y)=="object") && x.length == y.length){
+            let total = 0;
+            for (let index = 0; index < x.length; index++) {
+               let aux = x[index] * y[index];
+                total += aux;
+            }
+        }else
+            throw new Error("ninguna es correcta");
+              
+    }
